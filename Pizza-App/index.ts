7ef7d223a64ff4929411fedfbf5396ada54b67a8
Maria Lukowich <mlukowich27@gmail.com>
2024-10-15 13:42:20 -0600

    creating id function

--- Pizza-App/index.ts
+++ Pizza-App/index.ts
@@ -10,20 +10,23 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1,name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-]
-
 let cashInRegister = 100
 let nextOrderId = 1
+let nextPizzaId = 1
 const orderQueue: Order[] = []
+
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
 
 function addNewPizza(pizzaObj: Pizza): void {
     menu.push(pizzaObj)
 }
+
 
 function placeOrder(pizzaName: string): Order | undefined{
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
@@ -47,7 +50,7 @@ function completeOrder(orderId: number): Order | undefined {
     return order
 }
 
-export function getPizzaDetail(identifier: string | number) {
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
     if (typeof identifier === "string") {
         return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
     } else if (typeof identifier === "number") {
@@ -57,9 +60,9 @@ export function getPizzaDetail(identifier: string | number) {
     }
 }
 
-addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
